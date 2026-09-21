0277a5a9a4b9181e615595a04365a1d5509cc3e7
JRA646 <149916056+JRA646@users.noreply.github.com>
2026-09-21 10:52:12 +0800

    refactor(anywork): switch app entrypoint to modern architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MarketplaceApp from './MarketplaceApp.tsx'
+import App from './app/App'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MarketplaceApp />
+    <App />
   </StrictMode>,
 )

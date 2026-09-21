6e07216e68401721196e1d6c1fd66337fb81b4d1
JRA646 <149916056+JRA646@users.noreply.github.com>
2026-09-21 13:11:00 +0800

    feat: add confirmation feedback for quote acceptance

--- src/pages/RequestDetailPage.tsx
+++ src/pages/RequestDetailPage.tsx
@@ -17,6 +17,7 @@ import { quotes as mockQuotes, requests as mockRequests, providers as mockProvid
 import { StatusBadge } from '../components/StatusBadge'
 import { ProviderCard } from '../components/ProviderCard'
 import { RequestPhotos } from '../components/RequestPhotos'
+import { showError, showSuccess } from '../lib/alerts'
 import {
   acceptQuote,
   getRequest,

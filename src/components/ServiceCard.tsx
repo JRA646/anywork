8dd5dd677cd8a89a3c6c10427720871f90a098f9
JRA646 <149916056+JRA646@users.noreply.github.com>
2026-09-21 11:26:40 +0800

    feat: add service photography and hover effects

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,16 +1,18 @@
 import { ArrowUpRight } from 'lucide-react'
 import type { Service } from '../types/marketplace'
 import { icons } from './Icons'
+import { serviceImageById } from '../data/media'
 
 export function ServiceCard({ service, onClick }: { service: Service; onClick?: () => void }) {
   const Icon = icons[service.icon as keyof typeof icons]
+  const image = serviceImageById[service.id]
   return (
-    <button className="serviceCardModern" onClick={onClick}>
+    <button className="serviceCardModern serviceCardImage" style={{ '--service-image': 'url("' + image + '")' } as React.CSSProperties} onClick={onClick}>
+      <div className="serviceCardPhoto" />
+      <div className="serviceCardShade" />
       <div className="serviceCardIcon"><Icon size={21} /></div>
       <div className="serviceCardTopline"><span>FROM {service.startingPrice}</span><ArrowUpRight size={18} /></div>
-      <h3>{service.title}</h3>
-      <p>{service.label}</p>
-      <span className="serviceCardDescription">{service.description}</span>
+      <div className="serviceCardContent"><h3>{service.title}</h3><p>{service.label}</p><span className="serviceCardDescription">{service.description}</span></div>
     </button>
   )
 }
